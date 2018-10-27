4e0cdb8c62c5a88ef6e9b4ecb35971e918829f29
likunlang <likunlang@foxmail.com>
2018-10-28 01:20:31 +0800

    更新代码

--- python/run.py
+++ python/run.py
@@ -1,7 +1,7 @@
 import os
-os.system("python ./sss.py")
+# os.system("python ./sss.py")
 # os.system("python ./sss2.py")
-# os.system("python ./sss3.py")
+os.system("python ./sss3.py")
 # os.system("python ./sss4.py")
 # os.system("python ./sss5.py")
 # os.system("python ./sss6.py")

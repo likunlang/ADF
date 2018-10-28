690dc4d37829470767c12ec870d78eaec9b862d3
lide <likunlang@foxmail.com>
2018-10-29 02:01:40 +0800

    ad test.py

--- links.js
+++ links.js
@@ -1,93 +1,78 @@
 const links = [
     //7_647 5830
     [
-        'http://trafficbank.idouzi.com/api/details?id=5bd49274d657fe7afb3b67d3&flowMainId=5830&t=1540658682&sign=0e823a7e179c11c1346df0b0362192e3',
-        'http://trafficbank.idouzi.com/api/details?id=5bd48ff7d657fe406d263a93&flowMainId=5830&t=1540658642&sign=f15923fc1b99dffdf79a8a154a696bd2',
-        'http://trafficbank.idouzi.com/api/details?id=5bd48ffcd657fe4ea02347a3&flowMainId=5830&t=1540658627&sign=b962a7d22b1a36623d55af16dc66d498',
+        'http://trafficbank.idouzi.com/api/details?id=5bd5ef84d657fe363261b9d3&flowMainId=5830&t=1540748234&sign=00f9bd1a60e6ac72adc4f694fc7dbd4e',
+        'http://trafficbank.idouzi.com/api/details?id=5bd5ef8bd657fe652c7078f3&flowMainId=5830&t=1540748224&sign=c71120e4fbede879336aa090bc2c6f33',
     ],
     //8_644 5894
     [
-        'http://trafficbank.idouzi.com/api/details?id=5bd49219d657fe64cc5359e3&flowMainId=5894&t=1540659226&sign=a66006251629f2dd7921bcbe307b8e99',
-        'http://trafficbank.idouzi.com/api/details?id=5bd4921cd657fe746b586db3&flowMainId=5894&t=1540659216&sign=5149b45e46f81a865b55194c172fe327',
-        'http://trafficbank.idouzi.com/api/details?id=5bd49222d657fe042a650ea3&flowMainId=5894&t=1540659205&sign=b830caa743dda3a4de02fcbcb49edff1',
+        'http://trafficbank.idouzi.com/api/details?id=5bd5ef46d657fe083e33b913&flowMainId=5894&t=1540748394&sign=691ca5b49cfbd21fcf32a55863935db7',
+        'http://trafficbank.idouzi.com/api/details?id=5bd5ef57d657fe191e371703&flowMainId=5894&t=1540748386&sign=d771a5c184cf6dc39bcf9b20732acaf9',
     ],
     //9_646 6141
     [
-        'http://trafficbank.idouzi.com/api/details?id=5bd4916ad657fe3fd55e2fa3&flowMainId=6141&t=1540659153&sign=7f5c9d560476e3a5571bc83ed2dc3a01',
-        'http://trafficbank.idouzi.com/api/details?id=5bd49173d657fe4eb12c1173&flowMainId=6141&t=1540659144&sign=fbb859d6eae2dd9801245a7f88dd59ad',
-        'http://trafficbank.idouzi.com/api/details?id=5bd49179d657fe6dda5e46b3&flowMainId=6141&t=1540659134&sign=77cdef867a08b097f6dee4e79f257fb7',
+        'http://trafficbank.idouzi.com/api/details?id=5bd5ee6cd657fe5d5f42bf13&flowMainId=6141&t=1540748345&sign=251b0682b5d3408e20f68a8c558eb8bd',
+        'http://trafficbank.idouzi.com/api/details?id=5bd5ee78d657fe7bc94c9423&flowMainId=6141&t=1540748334&sign=b7ecb23281aed9e72887526739c9ceec',
     ],
     //1 176*** 1125
     [
-        'http://trafficbank.idouzi.com/api/details?id=5bd48f18d657fe03a16cab53&flowMainId=1125&t=1540659088&sign=baa71532a1ea87f314140152182b3ebd',
-        'http://trafficbank.idouzi.com/api/details?id=5bd49262d657fe3e4f2f5ff3&flowMainId=1125&t=1540659042&sign=4450eea377aa87ab7bf3f3acce785ada',
-        'http://trafficbank.idouzi.com/api/details?id=5bd4926ad657fe5c1f151bd3&flowMainId=1125&t=1540659033&sign=7445d3679292cd7cebcbc460e78d4ebc',
+        'http://trafficbank.idouzi.com/api/details?id=5bd586bcd657fe672d699cf3&flowMainId=1125&t=1540748304&sign=2d98b8773cd9c46f3c0f23818d802257',
+        'http://trafficbank.idouzi.com/api/details?id=5bd586d6d657fe08425311e3&flowMainId=1125&t=1540748292&sign=86522e2393f958fdb9dfd766653825b1',
     ],
     //2 138*** 5188
     [
-        'http://trafficbank.idouzi.com/api/details?id=5bd48098d657fe3894417873&flowMainId=5188&t=1540657815&sign=7cd040f64a0976c24af048fc98e86c3b',
-        'http://trafficbank.idouzi.com/api/details?id=5bd4809ed657fe4736278b93&flowMainId=5188&t=1540657806&sign=042a0d23b1a914c01575e4db0c3ec15d',
-        'http://trafficbank.idouzi.com/api/details?id=5bd480a3d657fe56eb2b90a3&flowMainId=5188&t=1540657795&sign=ec29080b17b502df87615601bb9bcfe0',
+        'http://trafficbank.idouzi.com/api/details?id=5bd5e00fd657fe51482139e3&flowMainId=5188&t=1540747705&sign=415b5549eb616cfcf5e1419fd04a5712',
+        'http://trafficbank.idouzi.com/api/details?id=5bd5e02ed657fe2f5831c933&flowMainId=5188&t=1540747678&sign=420cb702b0fe116eef407797ce862986',
     ],
     //B1 5207
     [
-        'http://trafficbank.idouzi.com/api/details?id=5bd492ced657fe24df0d20a3&flowMainId=5207&t=1540657926&sign=2791d14951823c2404dc22d8358436ff',
-        'http://trafficbank.idouzi.com/api/details?id=5bd492d6d657fe35ce39e053&flowMainId=5207&t=1540657913&sign=b55f8425ac5b448a2c413e2eaa6c7670',
-        'http://trafficbank.idouzi.com/api/details?id=5bd492dbd657fe44a02fe843&flowMainId=5207&t=1540657899&sign=5d989bbff0082924ea5f275a40945533',
+        'http://trafficbank.idouzi.com/api/details?id=5bd5e4e5d657fe6e2c7afdc3&flowMainId=5207&t=1540747744&sign=dc1fb31727d4cfd9f70f972efcc59a00',
+        'http://trafficbank.idouzi.com/api/details?id=5bd5e026d657fe105c3e8e23&flowMainId=5207&t=1540747731&sign=55545df2a7616d73c144a6c4b0b1041e',
     ],
     //B2 5286
     [
-        'http://trafficbank.idouzi.com/api/details?id=5bd49302d657fe4162040173&flowMainId=5286&t=1540657969&sign=b18e57bf40699ca7138834fcd38238b5',
-        'http://trafficbank.idouzi.com/api/details?id=5bd49306d657fe5095185983&flowMainId=5286&t=1540657960&sign=7e5c5808fc8344199d0428291a84abad',
-        'http://trafficbank.idouzi.com/api/details?id=5bd4930ad657fe60fd5fcd03&flowMainId=5286&t=1540657951&sign=60079d72c1130ea4d6afe5d1fb06d91c',
+        'http://trafficbank.idouzi.com/api/details?id=5bd5e4d3d657fe305469f323&flowMainId=5286&t=1540747801&sign=aafb9600b895c009a09a75f29831b4e5',
+        'http://trafficbank.idouzi.com/api/details?id=5bd5e4d7d657fe3f684d3213&flowMainId=5286&t=1540747790&sign=01e39f18447535739ab50a3a0114c241',
     ],
     //黑 5295
     [
-        'http://trafficbank.idouzi.com/api/details?id=5bd480a3d657fe56eb2b90a3&flowMainId=5295&t=1540657871&sign=fe84c5540bae692432e2da3e9a479823',
-        'http://trafficbank.idouzi.com/api/details?id=5bd49299d657fe79011b2403&flowMainId=5295&t=1540657861&sign=d107e4a55da74b1363fffde453100514',
-        'http://trafficbank.idouzi.com/api/details?id=5bd4929ed657fe095010b803&flowMainId=5295&t=1540657851&sign=a783bddae5ed00eaff7141cb01dfe1b3',
+        'http://trafficbank.idouzi.com/api/details?id=5bd5f095d657fe253c1435f3&flowMainId=5295&t=1540747857&sign=b00759c4f7824881dcf8036c08c51764',
+        'http://trafficbank.idouzi.com/api/details?id=5bd5f0a9d657fe36461381d3&flowMainId=5295&t=1540747847&sign=b29a379744521d7bd7850cd4116d223c',
     ],
     //团 5285
     [
-        'http://trafficbank.idouzi.com/api/details?id=5bd4932fd657fe3f24383c13&flowMainId=5285&t=1540658042&sign=2c3ccfd71b45193afa7bd69deed7c649',
-        'http://trafficbank.idouzi.com/api/details?id=5bd4933cd657fe5de534add3&flowMainId=5285&t=1540658032&sign=5af24665ad00be07844c0a9419f0c910',
-        'http://trafficbank.idouzi.com/api/details?id=5bd4934ad657fe0cad0881e3&flowMainId=5285&t=1540658023&sign=7a77d2a6b36fc77fd61e988acaff41ff',
+        'http://trafficbank.idouzi.com/api/details?id=5bd5e4b3d657fe52172611b3&flowMainId=5285&t=1540747956&sign=5e99cea8960a9a68da5803a062069587',
+        'http://trafficbank.idouzi.com/api/details?id=5bd5e4b3d657fe52172611b3&flowMainId=5285&t=1540747956&sign=5e99cea8960a9a68da5803a062069587',
     ],
     //1_642 5683
     [
-        'http://trafficbank.idouzi.com/api/details?id=5bd493b0d657fe5fe7520233&flowMainId=5683&t=1540658149&sign=f21e29ea9d3c9860752d3715ce8ccbb4',
-        'http://trafficbank.idouzi.com/api/details?id=5bd493b6d657fe710c292193&flowMainId=5683&t=1540658138&sign=cf2db9bb3a7d017e591b4fd229a92caa',
-        'http://trafficbank.idouzi.com/api/details?id=5bd493bcd657fe7fed453bd3&flowMainId=5683&t=1540658128&sign=461596d88d0c2c1d2928ceeebc8802b0',
+        'http://trafficbank.idouzi.com/api/details?id=5bd5e4c7d657fe11d8090783&flowMainId=5683&t=1540747900&sign=4b33c28c4801b42fbe498d88da1bd634',
+        'http://trafficbank.idouzi.com/api/details?id=5bd5e4ccd657fe212908f943&flowMainId=5683&t=1540747890&sign=74ce92ce2fb433d48212ffb60077e04d',
     ],
     //2_648 5667
     [
-        'http://trafficbank.idouzi.com/api/details?id=5bd49001d657fe5e9f2a6b23&flowMainId=5667&t=1540658219&sign=b7415939365660e7d78f25e7ca459386',
-        'http://trafficbank.idouzi.com/api/details?id=5bd493d0d657fe105a339d43&flowMainId=5667&t=1540658207&sign=e1429c5a925591b4ffc8c38c552be982',
-        'http://trafficbank.idouzi.com/api/details?id=5bd493e6d657fe5e0b11a883&flowMainId=5667&t=1540658197&sign=ceed030961b0f407c682703025fb6066',
+        'http://trafficbank.idouzi.com/api/details?id=5bd5e49ad657fe0668507ba3&flowMainId=5667&t=1540748016&sign=c87eb4dfe01f4e153f1a63616cfd5faf',
+        'http://trafficbank.idouzi.com/api/details?id=5bd5c0e8d657fe63a203fed3&flowMainId=5667&t=1540748761&sign=d59cd86aa05567b2d660f117050ac73f',
     ],
     //3_674 5688
     [
-        'http://trafficbank.idouzi.com/api/details?id=5bd49353d657fe2a5a4d8d63&flowMainId=5688&t=1540658102&sign=9cc7213fad398e6a6e15c6b389abd17d',
-        'http://trafficbank.idouzi.com/api/details?id=5bd4935cd657fe49b951b003&flowMainId=5688&t=1540658092&sign=8ce8d7ffbe34a540d8eaa04cf4362aaa',
-        'http://trafficbank.idouzi.com/api/details?id=5bd49389d657fe689648c2c3&flowMainId=5688&t=1540658078&sign=ce0eae6dabd77a6b1c0196eb37d4b508',
+        'http://trafficbank.idouzi.com/api/details?id=5bd5efa6d657fe335f1c3123&flowMainId=5688&t=1540748057&sign=d5276ddffd8c71809a80a1634f07c109',
+        'http://trafficbank.idouzi.com/api/details?id=5bd5efb7d657fe424b7c6113&flowMainId=5688&t=1540748047&sign=f98fbb45ccacefe23af28d45e4bf3d9b',
     ],
     //4_654 5703
     [
-        'http://trafficbank.idouzi.com/api/details?id=5bd493e6d657fe5e0b11a883&flowMainId=5703&t=1540658401&sign=730f7c0487a76ee2221818a4225c35bc',
-        'http://trafficbank.idouzi.com/api/details?id=5bd49441d657fe080f458a93&flowMainId=5703&t=1540658285&sign=a7cacb335b57616e7f3256241be36468',
-        'http://trafficbank.idouzi.com/api/details?id=5bd4944bd657fe363c69de73&flowMainId=5703&t=1540658275&sign=a9a820af6bde6c4679d89a8c7dd0e1b3',
+        'http://trafficbank.idouzi.com/api/details?id=5bd5f086d657fe76c4302793&flowMainId=5703&t=1540748093&sign=1915b46740a0c869f31d551bcef2e74a',
+        'http://trafficbank.idouzi.com/api/details?id=5bd5f08dd657fe070025fe03&flowMainId=5703&t=1540748084&sign=cbae45f1223a0f6aab379fb03d45dab3',
     ],
     //5_664 5731
     [
-        'http://trafficbank.idouzi.com/api/details?id=5bd30adbd657fe69a35f5f53&flowMainId=5731&t=1540558323&sign=d09ce6ebc155dad8ee75dfc829dcbc01',
-        'http://trafficbank.idouzi.com/api/details?id=5bd49479d657fe538d118f03&flowMainId=5731&t=1540658515&sign=3d906d820c66222333549cd0ebf4cea5',
-        'http://trafficbank.idouzi.com/api/details?id=5bd4947cd657fe6397278423&flowMainId=5731&t=1540658504&sign=0a35032f43d82ed6eccce5da941b250e',
+        'http://trafficbank.idouzi.com/api/details?id=5bd5ee99d657fe6aac4e3253&flowMainId=5731&t=1540748152&sign=7854d83d9a9f6a9f4c728b87a806a946',
+        'http://trafficbank.idouzi.com/api/details?id=5bd5eea2d657fe0a4e1b0d53&flowMainId=5731&t=1540748143&sign=6018dc3680505ae3e85830fed69e3e98',
     ],
     //6_643 5765
     [
-        'http://trafficbank.idouzi.com/api/details?id=5bd49460d657fe047f5d02c3&flowMainId=5765&t=1540658585&sign=5119ab6fb8b1b6d9f3603bfdaa259566',
-        'http://trafficbank.idouzi.com/api/details?id=5bd49466d657fe13380d11c3&flowMainId=5765&t=1540658575&sign=d911ad6766ac9b9b39db79683778f864',
-        'http://trafficbank.idouzi.com/api/details?id=5bd4946cd657fe24140de873&flowMainId=5765&t=1540658565&sign=62d0a9210b2b33a4ea6e6fe6e6b2aead',
+        'http://trafficbank.idouzi.com/api/details?id=5bd5f074d657fe38155b56d3&flowMainId=5765&t=1540748191&sign=551a396221f4b8f64b64c8d082cc6b2d',
+        'http://trafficbank.idouzi.com/api/details?id=5bd5f07ed657fe58360e7583&flowMainId=5765&t=1540748182&sign=dbf7688558bc52aa76908051ca8fc864',
     ],
 ];
 

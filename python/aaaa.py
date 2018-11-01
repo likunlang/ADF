bbc76ff5be3eca15a2a098fddc7bebafabbe06b9
likunlang <likunlang@foxmail.com>
2018-11-01 20:17:26 +0800

    删除python多余文件

--- python/aaaa.py
+++
@@ -1,6 +0,0 @@
-from selenium import webdriver 
-mobileEmulation = {'deviceName': 'iPhone X'}
-options = webdriver.ChromeOptions()
-options.add_experimental_option('mobileEmulation', mobileEmulation)
-driver = webdriver.Chrome(executable_path='chromedriver.exe', chrome_options=options)
-driver.get('http://m.baidu.com')
